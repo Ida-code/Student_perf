c20844867b5a2ed326eaeaf12f1936905d058a9d
Ida Sharon <idasharon.r@gmail.com>
2026-02-21 18:08:51 +0530

    Update database password in db.php

--- db.php
+++ db.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 $host = "localhost";
 $user = "root";
-$pass = "Risenme@20";
+$pass = "your pswd";
 $db   = "Student_Perf";
 
 $conn = new mysqli($host, $user, $pass, $db);
