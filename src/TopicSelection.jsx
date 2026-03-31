b5dab224faa04c28fa702a792c3fa43c56ce678a
Ida <idasharon.r@gmail.com>
2026-04-01 00:39:04 +0530

    Improved the UI of TopicSelection Page

--- src/TopicSelection.jsx
+++ src/TopicSelection.jsx
@@ -64,7 +64,7 @@ function TopicSelection() {
             fontSize: "1.1em",
           }}
         >
-          Complete each topic to unlock your potential
+          Complete each topic to unlock your potential!
         </p>
 
         <div
