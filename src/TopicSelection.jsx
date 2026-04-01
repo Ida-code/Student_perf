d142eafa706efd0f901a472568779d5b37131a29
Ida <idasharon.r@gmail.com>
2026-04-01 15:48:19 +0530

    topicselection updated

--- src/TopicSelection.jsx
+++ src/TopicSelection.jsx
@@ -55,7 +55,7 @@ function TopicSelection() {
             textShadow: "2px 2px 4px rgba(0,0,0,0.3)",
           }}
         >
-          Pick Your Topic to Master
+          Choose a Topic to Master
         </h1>
         <p
           style={{
